37d85e8e26f3fa135bf4b8ea06ad4655b853bfc3
Peace_Mathavhathi <vutshilomathavathi@gmail.com>
2023-08-12 14:02:06 +0200

    correcting spelling

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <p>Peacematha SofTech (PTY) LTD website is still under construction and comming soon!</p>
+      <p>Peacematha SofTech (PTY) LTD website is still under construction and coming soon!</p>
 
         <img style={{ width: 250, height: 200 ,paddingBottom:50}} src={require('./images/PEACEMATHA_SOFTECH_Logo.jpg')} alt="React Logo" />
         <img style={{ width: 150, height: 150, paddingup:150 }} src={logo} className="App-logo" alt="logo" />
