efd21786c8c1469bd178fd7d60dde236092c559f
Peacematha <39997491+peacematha@users.noreply.github.com>
2024-09-11 21:55:59 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
       <p>Peacematha SofTech (PTY) LTD website is still under construction and coming soon!</p>
 
-        <img style={{ width: 250, height: 200 ,paddingBottom:50}} src={require('./images/PEACEMATHA_SOFTECH_Logo.jpg')} alt="React Logo" />
+        <img style={{ width: 250, height: 200 ,paddingBottom:50}} src={require('./images/Lobola_Invitation.jpg')} alt="React Logo" />
         <img style={{ width: 150, height: 150, paddingup:50 }} src={logo} className="App-logo" alt="logo" />
         <h9>(2023/924131/07)</h9>
       </header>
