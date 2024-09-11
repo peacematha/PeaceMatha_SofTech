72c17f9dbf7d4234bef3fe9f9d29dd26b86b3031
Peacematha <39997491+peacematha@users.noreply.github.com>
2024-09-11 22:40:46 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
 
-        <img style={{ width: 850, height: 970}} src={require('./images/Lobola_Invitation.jpg')} alt="logo" />
+        <img style={{ width: 770, height: 970}} src={require('./images/Lobola_Invitation.jpg')} alt="logo" />
         <img style={{ width: 0, height: 0 }} src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
