da81ee645faeec73bfe5f585060caa6617ef8e3a
Peacematha <39997491+peacematha@users.noreply.github.com>
2024-09-14 21:00:28 +0200

    Update App.js
    Changing the file Extension

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
 
-        <img style={{ width: 570, height: 850}} src={require('./images/FinalCard.jpg')} alt="logo" />
+        <img style={{ width: 570, height: 850}} src={require('./images/FinalCard.png')} alt="logo" />
         <img style={{ width: 0, height: 0 }} src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
