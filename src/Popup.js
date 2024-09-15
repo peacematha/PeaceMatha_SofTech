a4c63c39ad363711bc856e1e850f798295b034a5
Peacematha <39997491+peacematha@users.noreply.github.com>
2024-09-15 13:36:02 +0200

    Add files via upload

---
+++ src/Popup.js
@@ -0,0 +1,17 @@
+import React from "react"
+import './Popup.css'
+
+function Popup(props){
+    return(props.trigger) ? (
+        <div className="style_1">
+        <div className="popup">
+            <div className ="popup-inner">
+                <button className="close-btn" onClick={()=>props.setTrigger(false)}>Close Information</button>
+                { props.children }
+           </div>
+        </div>
+    </div>
+    ) : "";
+}
+
+export default Popup 
